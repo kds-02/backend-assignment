e29b1651f3d55838d96393535ac543dea2b8dddb
kds20212962 <rlaehdtls@kookmin.ac.kr>
2025-12-18 17:28:02 +0900

    비밀번호 재수정

--- src/main/java/com/codedrill/shoppingmall/user/dto/SignupRequest.java
+++ src/main/java/com/codedrill/shoppingmall/user/dto/SignupRequest.java
@@ -16,9 +16,12 @@ public class SignupRequest {
     private String email;
 
 
-    @NotBlank
-    @Size(min = 8, max = 64)
-    @Pattern(regexp = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[!@#$%^&*()_+\\-={}\\[\\]:;\"'<>,.?/\\\\|]).+$")
+    @NotBlank(message = "비밀번호는 필수입니다.")
+    @Size(min = 8, message = "비밀번호는 최소 8자 이상이어야 합니다.")
+    @Pattern(
+            regexp = "^(?=\\S+$)(?=.*[A-Za-z])(?=.*\\d)(?=.*[^A-Za-z0-9]).+$",
+            message = "비밀번호는 영문자, 숫자, 특수기호를 각각 1개 이상 포함해야 합니다."
+    )
     private String password;
 
     @NotBlank
